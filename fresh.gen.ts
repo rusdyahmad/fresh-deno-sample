8941381c70b1756b6fc8d1fdd331578db984cb82
rusdyahmad <rusdyahmad@users.noreply.github.com>
2022-07-08 03:51:48 +0800

    Update fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,12 +3,14 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/about.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/github/[username].tsx";
-import * as $4 from "./routes/index.tsx";
-import * as $5 from "./routes/posts.tsx";
-import * as $6 from "./routes/posts/[id].tsx";
+import * as $1 from "./routes/_404.tsx";
+import * as $2 from "./routes/_500.tsx";
+import * as $3 from "./routes/about.tsx";
+import * as $4 from "./routes/api/joke.ts";
+import * as $5 from "./routes/github/[username].tsx";
+import * as $6 from "./routes/index.tsx";
+import * as $7 from "./routes/posts.tsx";
+import * as $8 from "./routes/posts/[id].tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/Footer.tsx";
 import * as $$2 from "./islands/Header.tsx";
@@ -16,12 +18,14 @@ import * as $$2 from "./islands/Header.tsx";
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
-    "./routes/about.tsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/github/[username].tsx": $3,
-    "./routes/index.tsx": $4,
-    "./routes/posts.tsx": $5,
-    "./routes/posts/[id].tsx": $6,
+    "./routes/_404.tsx": $1,
+    "./routes/_500.tsx": $2,
+    "./routes/about.tsx": $3,
+    "./routes/api/joke.ts": $4,
+    "./routes/github/[username].tsx": $5,
+    "./routes/index.tsx": $6,
+    "./routes/posts.tsx": $7,
+    "./routes/posts/[id].tsx": $8,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
